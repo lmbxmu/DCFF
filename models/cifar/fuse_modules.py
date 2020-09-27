33b986b9aa753ac62e5a0497922b93699077925f
bohong chen <32871610+chenbong@users.noreply.github.com>
2020-09-27 20:18:51 +0800

    Update fuse_modules.py

--- models/cifar/fuse_modules.py
+++ models/cifar/fuse_modules.py
@@ -20,30 +20,41 @@ class FuseConv2d(nn.Conv2d):
         cin = self.weight.shape[1]
         k = self.weight.shape[2]
 
-        for filteri in range(cout):
-            if filteri == 0:
-                fused_filteri_weight = (self.weight.reshape(cout, -1) * self.layeri_softmaxP[filteri].unsqueeze(dim=1).cuda()).sum(0).unsqueeze(dim=0)
-                fused_layeri_weight = fused_filteri_weight
+        # for filteri in range(cout):
+        #     if filteri == 0:
+        #         fused_filteri_weight = (self.weight.reshape(cout, -1) * self.layeri_softmaxP[filteri].unsqueeze(dim=1).cuda()).sum(0).unsqueeze(dim=0)
+        #         fused_layeri_weight = fused_filteri_weight
 
-                fused_filteri_bias = (self.bias * self.layeri_softmaxP[filteri].cuda()).sum(0).unsqueeze(dim=0)
-                fused_layeri_bias = fused_filteri_bias
+        #         fused_filteri_bias = (self.bias * self.layeri_softmaxP[filteri].cuda()).sum(0).unsqueeze(dim=0)
+        #         fused_layeri_bias = fused_filteri_bias
 
-            else:
-                fused_filteri_weight = (self.weight.reshape(cout, -1) * self.layeri_softmaxP[filteri].unsqueeze(dim=1).cuda()).sum(0).unsqueeze(dim=0)
-                fused_layeri_weight = torch.cat((fused_layeri_weight, fused_filteri_weight), 0)
+        #     else:
+        #         fused_filteri_weight = (self.weight.reshape(cout, -1) * self.layeri_softmaxP[filteri].unsqueeze(dim=1).cuda()).sum(0).unsqueeze(dim=0)
+        #         fused_layeri_weight = torch.cat((fused_layeri_weight, fused_filteri_weight), 0)
 
-                fused_filteri_bias = (self.bias * self.layeri_softmaxP[filteri].cuda()).sum(0).unsqueeze(dim=0)
-                fused_layeri_bias = torch.cat((fused_layeri_bias, fused_filteri_bias), 0)
+        #         fused_filteri_bias = (self.bias * self.layeri_softmaxP[filteri].cuda()).sum(0).unsqueeze(dim=0)
+        #         fused_layeri_bias = torch.cat((fused_layeri_bias, fused_filteri_bias), 0)
+        # print(self.layeri_softmaxP)
+        # print(self.layeri_softmaxP.unsqueeze(1))
+        # print(self.layeri_softmaxP.unsqueeze(1).expand(-1, 27, -1).shape)
+        # print(self.weight.reshape(cout, -1).unsqueeze(2).expand(-1, -1, cout).shape)
+        # exit(0)
+        fused_layeri_weight = (self.weight.reshape(cout, -1).unsqueeze(2).expand(-1, -1, cout) * self.layeri_softmaxP.unsqueeze(1).expand(-1, cin*k*k, -1)).sum(dim=2)
 
+        fused_layeri_weight = torch.index_select(fused_layeri_weight, 0, self.layeri_topm_filters_id)
+
+        # print(fused_layeri_weight.shape)
         fused_layeri_weight = fused_layeri_weight.reshape(cout, cin, k, k)
 
         # set the weight (not in top m) to zero
-        for filters in range(self.weight.shape[0]):
-            if filters not in self.layeri_topm_filters_id:
-                # with torch.no_grad():
-                fused_layeri_weight[filters].mul_(0)
-                fused_layeri_bias[filters].mul_(0)
+        # for filters in range(self.weight.shape[0]):
+        #     if filters not in self.layeri_topm_filters_id:
+        #         # with torch.no_grad():
+        #         fused_layeri_weight[filters].mul_(0)
+        #         fused_layeri_bias[filters].mul_(0)
+
+
 
         output = F.conv2d(input=input, weight=fused_layeri_weight, bias=fused_layeri_bias, stride= self.stride, padding= self.padding, dilation= self.dilation, groups= self.groups)
-
+        exit(0)
         return output
