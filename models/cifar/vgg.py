034102a6575882c25a1a2721bfab8791e65e17cc
bohong chen <32871610+chenbong@users.noreply.github.com>
2020-09-28 09:03:47 +0800

    Update vgg.py

--- models/cifar/vgg.py
+++ models/cifar/vgg.py
@@ -1,20 +1,21 @@
 import torch
 import torch.nn as nn
+
 from .fuse_modules import *
 
+
 cfg = {
-    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
     'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
-    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M',512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
 }
 
-class VGG(nn.Module):
-    def __init__(self, vgg_name, num_class=10):
-        super(VGG, self).__init__()
-        self.features = self._make_layers(cfg[vgg_name])
-        self.classifier = nn.Linear(512, 10)
 
+class VGG(nn.Module):
+    def __init__(self, vgg_name, cprate, num_class=10):
+        super(VGG, self).__init__()
+
+        self.features = self._make_layers(cfg[vgg_name], cprate)
+        cout = int(512*(1-cprate[-1]))
+        self.classifier = nn.Linear(cout, 10)
 
     def forward(self, x):
         out = self.features(x)
@@ -22,37 +23,23 @@ class VGG(nn.Module):
         out = self.classifier(out)
         return out
     
-    def _make_layers(self, cfg):
+    def _make_layers(self, cfg, cprate):
         layers = []
         in_channels = 3
-        for x in cfg:
+        conv_layerid = 0
+        for layeri, x in enumerate(cfg):
             if x == 'M':
                 layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
             else:
-                # layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                cout = int(x*(1-cprate[conv_layerid]))
                 layers += [FuseConv2d(in_channels, x, kernel_size=3, padding=1),
-                           nn.BatchNorm2d(x),
+                           nn.BatchNorm2d(cout),
                            nn.ReLU(inplace=True)]
-                in_channels = x
+                in_channels = cout
+                conv_layerid += 1
+
         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+
         return nn.Sequential(*layers)
 
-def VGG11():
-    return VGG('vgg11')
 
-def VGG13():
-    return VGG('vgg13')
-
-def VGG16():
-    return VGG('vgg16')
-
-def VGG19():
-    return VGG('vgg19')
-
-# def test():
-#     model = VGG16()
-#     x = torch.randn(2,3,32,32)
-#     y = model(x)
-#     print(y.size())
-
-# test()
