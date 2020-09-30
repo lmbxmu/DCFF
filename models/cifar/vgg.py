3ade0e44a79409b4c1790b00ec9bec3f2ffe7aa4
bohong chen <32871610+chenbong@users.noreply.github.com>
2020-09-30 12:52:37 +0800

    Update vgg.py

--- models/cifar/vgg.py
+++ models/cifar/vgg.py
@@ -1,17 +1,19 @@
 import torch
 import torch.nn as nn
 
-from .fuse_modules import *
+from .fuse_modules import FuseConv2d
+# from fuse_modules import FuseConv2d
 
 
 cfg = {
     'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
 }
 
-
-class VGG(nn.Module):
+#*================================
+#* fused model
+class Fused_VGG(nn.Module):
     def __init__(self, vgg_name, cprate, num_class=10):
-        super(VGG, self).__init__()
+        super(Fused_VGG, self).__init__()
 
         self.features = self._make_layers(cfg[vgg_name], cprate)
         cout = int(512*(1-cprate[-1]))
@@ -43,3 +45,50 @@ class VGG(nn.Module):
         return nn.Sequential(*layers)
 
 
+
+#*================================
+#* compact model
+class Compact_VGG(nn.Module):
+    def __init__(self, vgg_name, cprate, num_class=10):
+        super(Compact_VGG, self).__init__()
+
+        self.features = self._make_layers(cfg[vgg_name], cprate)
+        cout = int(512*(1-cprate[-1]))
+        self.classifier = nn.Linear(cout, 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+    
+    def _make_layers(self, cfg, cprate):
+        layers = []
+        in_channels = 3
+        conv_layerid = 0
+        for layeri, x in enumerate(cfg):
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                cout = int(x*(1-cprate[conv_layerid]))
+                # layers += [FuseConv2d(in_channels, x, kernel_size=3, padding=1),
+                layers += [nn.Conv2d(in_channels, cout, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(cout),
+                           nn.ReLU(inplace=True)]
+                in_channels = cout
+                conv_layerid += 1
+
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+
+        return nn.Sequential(*layers)
+
+
+
+# def test():
+#     cprate = [0.7]*7+[0.1]*6
+    # model = Compact_VGG('vgg16', cprate)
+#     x = torch.randn(2,3,32,32)
+#     y = model(x)
+#     print(y.size())
+
+# test()
