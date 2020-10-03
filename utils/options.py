8db4e7d166d41bcd6213ddcdc5d9fda2399aa94d
bohong chen <32871610+chenbong@users.noreply.github.com>
2020-10-03 22:47:45 +0800

    Update options.py

--- utils/options.py
+++ utils/options.py
@@ -6,18 +6,18 @@ parser = argparse.ArgumentParser(description='Pytorch ImageNet Training')
 parser.add_argument(
     '--data_set',
     type=str,
-    # default='imagenet',
-    default='cifar10',
+    default='imagenet',     # for imagenet
+    # default='cifar10',      # for cifar10
     help='Name of dataset. [cifar10, imagenet] default: '
 )
 
 parser.add_argument(
     '--data_path',
     type=str,
-    # default='/media/disk2/zyc/ImageNet2012/',
-    # default='/home/cbh/one/paper-code/Dataset/ImageNet2012_sample',
-    default='/home/cbh/one/Dataset',
-    help='Path to dataset. default: '
+    default='/media/disk2/data/ImageNet2012', # for imagenet 14
+    # default='/media/disk1/lishaojie/ImageNet2012', # for imagenet 22
+    # default='/home/cbh/one/Dataset',  # for cifar10
+    help='Path to dataset.'
 )
 
 
@@ -25,22 +25,22 @@ parser.add_argument(
 parser.add_argument(
     '--train_batch_size',
     type=int,
-    default=256,
-    help='Batch size of all GPUS. default: 32'
+    default=128,
+    help='Batch size of all GPUS.'
 )
 
 parser.add_argument(
     '--test_batch_size',
     type=int,
-    default=256,
-    help='Batch size of all GPUS. default: 16'
+    default=128,
+    help='Batch size of all GPUS.'
 )
 
 parser.add_argument(
     '--num_epochs',
     type=int,
-    default=150,    # for cifar10
-    # default=90,     # for imagenet
+    # default=150,    # for cifar10
+    default=90,     # for imagenet
     help='Num of total epochs to run. default: 10'
 )
 
@@ -55,7 +55,7 @@ parser.add_argument(
 parser.add_argument(
     '--lr',
     type=float,
-    default=0.01,   # for cifar10
+    default=0.01,   # for cifar10/imagenet
     help='Learning rate. default: 0.01'
 )
 
@@ -63,21 +63,31 @@ parser.add_argument(
     '--lr_decay_step',
     type=int,
     nargs='+',
-    default=[50, 100],    # for cifar10
+    # default=[50, 100],    # for cifar10
+    default=None,           # for imagenet
     help='The iterval of learning rate decay (for cifar10). default: [50, 100]'
+)
+
+parser.add_argument(
+    '--lr_type',
+    default='step', 
+    type=str, 
+    help='lr scheduler (step/exp/cos/step3/fixed)'
 )
 
 parser.add_argument(
     '--momentum',
     type=float,
-    default=0.9,
+    # default=0.9,    # for cifar10
+    default=0.99,     # for imagenet
     help='Momentum. default: 0.9'
 )
 
 parser.add_argument(
     '--weight_decay',
     type=float,
-    default=5e-4,   # for cifar10
+    # default=5e-4,   # for cifar10
+    default=1e-4,   # for imagenet
     help='Weight decay.'
 )
 
@@ -86,7 +96,8 @@ parser.add_argument(
 parser.add_argument(
     '--arch',
     type=str,
-    default='vgg16',
+    # default='vgg16',    # for cifar10
+    default='resnet50', # for imagenet
     help='Model architecture. default: resnet18'
 )
 
@@ -113,13 +124,26 @@ parser.add_argument(
     help='The directory where the summaries will be sotred. default: ./experiment',
 )
 
+parser.add_argument(
+    '--reset',
+    default=False,
+    action='store_true',
+    help='reset the directory?'
+)
+
+parser.add_argument(
+    '--debug',
+    default=False,
+    action='store_true',
+    help='input to open debug state'
+)
 
 #* pruning-related
 parser.add_argument(
     '--cprate',
     type=str,
     default=None,
-    help='compress rate of each conv'
+    help='compress rate of each conv.'
 )
 
 parser.add_argument(
