4ad620b32f79c45ff856441eb742e4ff67efd692
bohong chen <32871610+chenbong@users.noreply.github.com>
2020-10-04 15:25:13 +0800

    Update fuse_modules.py

--- models/cifar/fuse_modules.py
+++ models/cifar/fuse_modules.py
@@ -23,11 +23,9 @@ class FuseConv2d(nn.Conv2d):
 
         fused_layeri_weight = torch.mm(self.layeri_softmaxP, self.weight.reshape(cout, -1))
         if self.bias is not None:
-            fused_layeri_bias = torch.mm(self.layeri_softmaxP, self.bias.unsqueeze(1)).squeeze()
+            fused_layeri_bias = torch.mm(self.layeri_softmaxP, self.bias.unsqueeze(1)).squeeze(1)
         else:
             fused_layeri_bias = self.bias
-
-        fused_layeri_weight = self.weight[:len(self.layeri_softmaxP),]
 
         fused_layeri_weight = fused_layeri_weight.reshape(-1, cin, k, k)
 
@@ -35,7 +33,8 @@ class FuseConv2d(nn.Conv2d):
         self.fused_weight = fused_layeri_weight
         self.fused_bias = fused_layeri_bias
 
-
         output = F.conv2d(input=input, weight=fused_layeri_weight, bias=fused_layeri_bias, stride=self.stride, padding=self.padding, dilation=self.dilation, groups=self.groups)
         return output
-    
+
+
+
